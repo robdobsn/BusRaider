13bc649f7fc86f1fa40ad4afd28853f5f1717c3c
W00ge <joe@dobson.name>
2018-05-18 08:46:33 +0100

    Tidied up slightly

---
+++ PiSw/src/piwiring.h
@@ -0,0 +1,10 @@
+#ifndef _PIBUSRAIDER_WIRING_H_
+#define _PIBUSRAIDER_WIRING_H_
+
+#include "utils.h"
+
+extern uint8_t convModeToVal(uint8_t mode);
+extern void pinMode(uint8_t pin, uint8_t mode);
+extern void digitalWrite(uint8_t pin, int val);
+
+#endif
