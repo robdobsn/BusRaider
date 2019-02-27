4a93a25ee4066d1d0e27729d7042a9a058d99902
Rob Dobson <robdobsn@users.noreply.github.com>
2019-02-27 17:12:38 +0000

    Completed Spectrum SNA format

--- PiSw/src/FileFormats/McZXSpectrumSNAFormat.h
+++ PiSw/src/FileFormats/McZXSpectrumSNAFormat.h
@@ -12,7 +12,8 @@ typedef struct {
 	uint8_t i;
 	uint16_t hl2, de2, bc2, af2;
 	uint16_t hl, de, bc;
-	uint8_t interrupt;
+	uint16_t ix, iy;
+	uint8_t iff2;
 	uint8_t r;
 	uint16_t af, sp;
 	uint8_t intMode;
@@ -42,8 +43,35 @@ class McZXSpectrumSNAFormat
 			return false;
 		}
 		sna_t *sna = (sna_t *) pData;
+		// Registers
+		Z80Registers regs;
+		regs.AF = sna->af;
+		regs.AFDASH = sna->af2;
+		regs.BC = sna->bc;
+		regs.BCDASH = sna->bc2;
+		regs.DE = sna->de;
+		regs.DEDASH = sna->de2;
+		regs.HL = sna->hl;
+		regs.HLDASH = sna->hl2;
+		regs.IX = sna->ix;
+		regs.IY = sna->iy;
+		regs.SP = sna->sp;
+		regs.I = sna->i;
+		regs.R = sna->r;
+		regs.INTMODE = sna->intMode & 0x03;
+		regs.INTENABLED = (sna->iff2 & 0x04) != 0;
+
+		// PC is pushed onto the stack so get the PC value and restore stack
+		uint32_t stackOffset = sna->sp - 0x4000;
+		uint16_t pcVal = sna->ramDump[stackOffset] + sna->ramDump[stackOffset+1]*256;
+		regs.SP++;
+		regs.SP++;
+		regs.PC = pcVal;
+		pRegsCallback(regs);
+
 		pDataCallback(16384, sna->ramDump, 49152); 
-		LogWrite(_logPrefix, LOG_DEBUG, "OK format\n");
+		LogWrite(_logPrefix, LOG_DEBUG, "format ok, PC %04x SP (after retn) %04x AF %04x HL %04x DE %04x BC %04x\n", 
+						regs.PC, regs.SP, regs.AF, regs.HL, regs.DE, regs.BC);
 		return true;
 	}
 };
