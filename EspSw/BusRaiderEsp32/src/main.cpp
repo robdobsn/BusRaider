6300ec23180d3ee8c56f2581483b13c3ba53215a
Rob Dobson <robdobsn@users.noreply.github.com>
2019-06-10 21:26:54 +0100

    Bump ESP32 version

--- EspSw/BusRaiderEsp32/src/main.cpp
+++ EspSw/BusRaiderEsp32/src/main.cpp
@@ -36,7 +36,7 @@
 const char* systemType = "BusRaiderESP32";
 
 // System version
-const char* systemVersion = "1.7.133";
+const char* systemVersion = "1.7.134";
 
 // Build date
 const char* buildDate = __DATE__;
